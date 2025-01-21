ecd968c13ddaf71fac38a181fb352fb9450d82fd
ayushkothari1 <pahadiayush61@gmail.com>
2025-01-21 21:47:39 +0530

    Some Other Examples

--- string1/4.js
+++ string1/4.js
@@ -3,5 +3,11 @@
 function longestWord (str){
     const words = str.split(' ');
     let longest = '';
-    
+    for (const word of words) {
+        if(word.length > longest.length){
+            longest = word;
+        }
+    }
+    return longest;
 }
+console.log(longestWord("i   am learning Javascript it doen't mean i don't have follow infact i want to address Elephant in the romm @hathiBhaiFamily"));
