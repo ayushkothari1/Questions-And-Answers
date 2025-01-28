6341cef58d7cba7da47ff3f625e8d9d3fe2eaf69
ayushkothari1 <pahadiayush61@gmail.com>
2025-01-28 14:12:17 +0530

    More Questions

---
+++ string1/7.js
@@ -0,0 +1,39 @@
+// charAt(index):
+
+
+// What would "hello".charAt(2) return?
+
+// console.log("hello".charAt(2));
+// console.log("hello".charAt(10));
+
+let str = "ram naam satya hai";
+
+// console.log(str.charAt(5));
+
+
+
+
+// What will "hello".charCodeAt(1) return?
+// Unicode
+// console.log("helloe".charCodeAt(5));
+// console.log("ayush".charCodeAt(0));
+
+
+
+
+// indexOf(searchValue, fromIndex):
+
+// What will "javascript".indexOf("a", 2) return?
+
+// console.log("javascript".indexOf("a", 3));
+// console.log("apple".indexOf("e"));
+
+
+
+
+// lastIndexOf(searchValue, fromIndex):
+
+// What will "banana".lastIndexOf("a") return?
+
+console.log("banana".lastIndexOf("a"));
+console.log("bananazzaaaaaaaaaa".lastIndexOf("a", 20));
---
+++ string1/8.js
@@ -0,0 +1,18 @@
+// includes(searchValue, fromIndex):
+
+// How would you check if the string "javascript" contains the substring "script" using includes()?
+
+let string = "javascript";
+let usedscript = string.includes("script");
+// console.log(usedscript);
+
+// console.log("hello".includes("o", 3));
+
+
+
+
+// startsWith(searchValue, fromIndex):
+// Does "hello".startsWith("he") return true or false?
+
+console.log("hello".startsWith("he"));
+console.log("hello world".startsWith("world"));
