84cf77a6694c286ab2461328553079361f3ad8bf
ayushkothari1 <pahadiayush61@gmail.com>
2025-01-30 21:12:40 +0530

    More Questions

--- string1/10.js
+++ string1/10.js
@@ -2,4 +2,23 @@
 
 // How would you extract "hel" from "hello" using slice()?
 
-console.log("hello".slice(0, 3));
+// console.log("hello".slice(0, 3));
+// console.log('javascript'.slice(4, 7));
+
+
+
+// substring(startIndex, endIndex):
+
+// How does "hello".substring(1, 4) differ from "hello".slice(1, 4)?
+
+// console.log("hello".substring(1, 4));
+// console.log("hello".slice(1, 4));
+// it swaps the number itslef unlike slice when start index is bigger then end index
+
+
+
+
+// substr(startIndex, length) (deprecated):
+// "hello".substr(0, 3) returns "hel".
+
+// console.log("hello".substr(0, 3));
