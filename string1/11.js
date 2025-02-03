df0d4a4f364a84b54a93ff54ca2aed95e5c58584
ayushkothari1 <pahadiayush61@gmail.com>
2025-02-03 20:08:08 +0530

    More Questions

--- string1/11.js
+++ string1/11.js
@@ -6,7 +6,31 @@
 
 
 // How can you split "apple,banana,orange" into just two elements using split()?
-console.log("apple,banana,orange".split(",", 2));
+// console.log("apple,banana,orange".split(",", 2));
 let fruits = "apple,banana,orange";
 let result = fruits.split(",", 2);
-console.log(result);  // Output: ["apple", "banana,orange"]
+// console.log(result);  // Output: ["apple", "banana,orange"]
+
+
+
+// trim
+// How does " hello ".trim() work?
+
+// console.log(" hello ".trim());
+// console.log(" rakm ki naiya hai pyar ke bharose  ".trim());
+// console.log("rakm ki naiya hai pyar ke bharose".replace("k", ""));
+// console.log("".trim());
+
+
+
+
+// trimStart()
+// How does " hello".trimStart() modify the string?
+// console.log(" hello".trimStart());
+// console.log("hello ".trimStart());
+
+
+
+// trimEnd()
+console.log("hello ".trimEnd());
+console.log(" hello".trimEnd());
---
+++ string1/12.js
@@ -0,0 +1,15 @@
+// UpperCase
+// What will "hello".toUpperCase() output?
+
+// console.log("hello".toUpperCase());
+// console.log("Ramav Raghvam".toUpperCase());
+// console.log("ritihara".toUpperCase());
+// console.log("javascript".toUpperCase());
+
+
+
+
+// toLowerCase
+// Does "abc".toLowerCase() return a new string or modify the original?
+
+console.log("abc".toLowerCase());
