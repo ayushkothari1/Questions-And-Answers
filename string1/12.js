e5e06a8c284ee75255cc7c05376bc5bb3bcbee21
ayushkothari1 <pahadiayush61@gmail.com>
2025-02-04 12:56:55 +0530

    More Questions

--- string1/12.js
+++ string1/12.js
@@ -13,3 +13,43 @@
 // Does "abc".toLowerCase() return a new string or modify the original?
 
 console.log("abc".toLowerCase());
+
+
+
+
+
+// localeCompare(compareString):
+// What does "apple".localeCompare("banana") return?
+
+console.log("apple".localeCompare("banana"));
+console.log("aeage".localeCompare("aeagaa"));
+console.log("cat".localeCompare("dog"));
+
+
+
+
+
+// valueOf():
+
+// What does "hello".valueOf() return?
+console.log("hello".valueOf());
+
+// Both .valueOf() and .toString() return the same value for strings ("hello"), but:
+
+// .valueOf() is meant to return the primitive value (which for strings is the string itself).
+// .toString() returns a string representation of the object, and for a string, it just returns the string itself as well.
+// Thus, for primitive types like strings, they behave the same.
+
+
+
+
+// toString
+// How can you convert the number 123 to a string?
+
+const num = 123;
+console.log(num.toString());
+console.log(typeof(num.toString()));
+
+// alling toString() does not alter the original string.
+
+// In JavaScript, strings are immutable, which means they cannot be changed once they are created. The toString() method just returns a new string (or the same string if it's already a string) and doesn't modify the original string.
---
+++ string1/13.js
@@ -0,0 +1,33 @@
+// match(regex):
+
+// How can you check if "hello" contains an "e" using match()?
+const str = "hello";
+const result = str.match(/e/);
+// console.log(result);
+// console.log(result != null);
+
+
+// What does "hello world".match(/\w+/) return?
+
+// console.log("ram world".match(/\w+/));
+
+
+
+// matchAll(regex):
+
+// What will "abracadabra".matchAll(/a/g) return?
+
+console.log("abracadabra".matchAll(/a/g));
+// for (const match of "abracadabra".matchAll(/a/g)) {
+//     console.log(match);
+//   }
+  
+  
+
+
+//   search(regex):
+
+// What does "hello world".search(/world/) return?
+
+console.log("ram jayanti".search(/jayanti/));
+console.log("ram".search(/b/));
