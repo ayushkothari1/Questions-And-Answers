f529b68684a79fac1a7d0d47d6c0101060558e10
ayushkothari1 <pahadiayush61@gmail.com>
2025-02-04 19:12:26 +0530

    More Questions

---
+++ Array/1.js
@@ -0,0 +1,34 @@
+// push(element1, ..., elementN):
+
+// What does arr.push(4) do to the array arr = [1, 2, 3]?
+
+const arr = [1, 2, 3];
+// console.log(arr);
+const result = arr.push(4);
+// console.log(arr);
+// console.log(result);
+
+const result2 = arr.push(5, 6);
+// console.log(arr);
+// console.log(result2);
+
+
+
+// pop();
+
+const arr2 = [1, 2, 3];
+// console.log(arr2.pop());
+// console.log(arr2);
+
+
+
+// unshift(element1, ..., elementN):
+
+// How does arr.unshift(0) affect arr = [1, 2, 3]?
+
+const ar = [1, 2, 4];
+
+const unshift1 = ar.unshift(0);
+console.log(unshift1);
+console.log(ar);
+
