b07b12eeea9516476ca5a2fa129d5184681a8b55
ayushkothari1 <pahadiayush61@gmail.com>
2025-11-20 15:16:24 +0530

    promise

--- Array/1.js
+++ Array/1.js
@@ -2,25 +2,21 @@
 
 // What does arr.push(4) do to the array arr = [1, 2, 3]?
 
-const arr = [1, 2, 3];
+// const arr = [1, 2, 3];
+// // console.log(arr);
+// const result = arr.push(45);
 // console.log(arr);
-const result = arr.push(4);
-console.log(arr);
-console.log(result);
+// console.log(result);
 
-const result2 = arr.push(5, 6);
+// const result2 = arr.push(5, 8, 6);
 // console.log(arr);
 // console.log(result2);
 
-
-
 // pop();
 
-const arr2 = [1, 2, 3];
-console.log(arr2.pop());
-console.log(arr2);
-
-
+// const arr2 = [1, 2, 34];
+// console.log(arr2.pop());
+// console.log(arr2);
 
 // unshift(element1, ..., elementN):
 
@@ -28,20 +24,14 @@ console.log(arr2);
 
 const ar = [1, 2, 4];
 
-const unshift1 = ar.unshift(0);
-console.log(unshift1);
+const unshift1 = ar.unshift(0, 8);
 console.log(ar);
-
+console.log(unshift1);
 
 const arr3 = [1, 2, 3];
 const result3 = arr3.unshift(-1, 0);
 console.log(result3);
 console.log(arr3);
-
-
-
-
-
 
 // shift()
 
@@ -54,3 +44,16 @@ console.log(arr4);
 console.log(result4);
 
 // shift() takes first index of array from array;
+
+let items = ["x", "y", "z"];
+let items2 = items.shift();
+// What is items?
+
+console.log(items);
+console.log(items2);
+
+let data = [];
+data.push(1);
+data.push(2);
+data.unshift(0);
+console.log(data);
