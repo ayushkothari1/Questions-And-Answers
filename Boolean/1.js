6cbc0c52e5b17f11296fc442d8774fbcc73d9e64
ayushkothari1 <pahadiayush61@gmail.com>
2025-12-04 18:04:26 +0530

    questions

--- Boolean/1.js
+++ Boolean/1.js
@@ -2,3 +2,22 @@
 
 let p = 2 == 2 ? "god is good" : "god is maybe not so good";
 console.log(p);
+
+const ektara = false ?? 0;
+
+console.log(ektara);
+
+console.log(Math.round(Math.random() * 10));
+console.log(Math.round(4.5));
+
+const max = 10;
+const min = 5;
+const randomNum = Math.floor(Math.random() * (max - min)) + min;
+console.log(randomNum);
+
+console.log(Math.sqrt(1369));
+console.log(Math.cbrt(1331));
+
+console.log(Math.abs(-34));
+
+console.log(Math.pow(11, 3));
