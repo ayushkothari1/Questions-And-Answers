1cdad93d2b564188012f4a9ca199204c0b8e8c1d
ayushkothari1 <pahadiayush61@gmail.com>
2025-12-05 12:28:51 +0530

    boolean usualy

---
+++ Numbers/2.js
@@ -0,0 +1,19 @@
+let num1 = parseFloat("-0.3fdksj33");
+console.log(num1);
+console.log(parseFloat("-3.4"));
+let num2 = parseInt("-0.3fdksj33");
+console.log(num2);
+console.log(parseInt("-3.44"));
+
+let hey = 3;
+let ra = ++hey;
+console.log(ra);
+
+let z = "3";
+z += 5;
+console.log(z);
+
+let a = 29;
+a %= 7;
+
+console.log(a);
