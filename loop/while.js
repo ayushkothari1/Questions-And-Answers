ad64f56ff852558cdfe81201f9a7f135d2a901ad
ayushkothari1 <pahadiayush61@gmail.com>
2026-02-13 00:50:45 +0530

    while

--- loop/while.js
+++ loop/while.js
@@ -1,15 +1,37 @@
 // Question: Write a program that prints the numbers from 1 to 5 using a while loop.
 
-let num = 1;
-while (num <= 5) {
-  console.log(num);
-  num++;
-}
+// let num = 1;
+// while (num <= 5) {
+//   console.log(num);
+//   num++;
+// }
 
 // Question: Write a program that prints the numbers from 1 to 5 using a do while loop.
 
-let index = 1;
+// let index = 1;
+// do {
+//   console.log(index);
+//   index++;
+// } while (index < 5);
+
+let counter = 0;
+while (counter < 5) {
+  console.log(counter);
+  counter++;
+}
+
+// One key difference between a do...while loop and a while loop is that the do...while loop will execute the block of code at least once before checking the condition.
+
+// If the condition is true, the block of code will continue to execute. If the condition is false, the block of code will stop executing.
+
+let raka = 5;
 do {
-  console.log(index);
-  index++;
-} while (index <= 5);
+  raka++;
+  console.log(raka);
+} while (raka < 2);
+
+let aka = 5;
+while (aka < 2) {
+  aka++;
+  console.log(aka);
+}
