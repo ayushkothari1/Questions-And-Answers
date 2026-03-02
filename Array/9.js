f7fcbdc554aac59bd0cb81611322a9524b2da40e
ayushkothari1 <pahadiayush61@gmail.com>
2026-03-02 22:05:05 +0530

    parseArray

---
+++ Array/9.js
@@ -0,0 +1,11 @@
+const array = new Array(5);
+console.log(array.length);
+console.log(array);
+
+const newArray = new Array(5).fill(20);
+console.log(newArray);
+console.log(newArray.length);
+
+const onceArrray = Array.from({ length: 4 });
+console.log(onceArrray);
+console.log(onceArrray.length);
