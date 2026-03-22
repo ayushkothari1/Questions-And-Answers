cdbc2fc58e9eeb1696011b2b9b87abe3a9515b0c
ayushkothari1 <pahadiayush61@gmail.com>
2026-03-22 20:38:21 +0530

    destroyer remover

--- function/Symmatric.js
+++ function/Symmatric.js
@@ -11,3 +11,14 @@ console.log(diffArray([3, 4, 3, 5], [1, 4, 2, 4]));
 console.log(diffArray([3, 6, 3, 5], [10, 3, 2, 4]));
 console.log(diffArray([3, 6, 13, 5], [10, 3, 2, 4]));
 console.log(diffArray([7, 6, 3, 5], [10, 13, 12, 4]));
+
+//
+// 2
+// Implement a Value Remover Function
+// In this lab, you will create a function that takes an initial array as its first argument, followed by one or more additional arguments representing the values to remove.
+
+function destroyer(arr, ...x) {
+  return arr.filter((y) => !x.includes(y));
+}
+
+console.log(destroyer([3, 4, 3, 5, 2, 5, 5, 6, 7], 2, 4, 5, 2, 6));
