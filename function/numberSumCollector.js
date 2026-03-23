e7a7640267bd52aea3b7b7b67163fa0e8eba91f9
ayushkothari1 <pahadiayush61@gmail.com>
2026-03-23 20:34:58 +0530

    numberSumcollector

---
+++ function/numberSumCollector.js
@@ -0,0 +1,29 @@
+// Build a Prime Number Sum Calculator
+//  a calculator that takes a number and returns the sum of all prime numbers that are less than or equal to that number.
+
+function sumPrimes(num) {
+  function isPrime(n) {
+    if (n < 2) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) return false;
+    }
+    return true;
+  }
+  let sum = 0;
+  if (num < 2) return 0;
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+console.log(sumPrimes(13));
+console.log(sumPrimes(17));
+console.log(sumPrimes(2));
+console.log(sumPrimes(12));
+console.log(sumPrimes(3));
+console.log(sumPrimes(2));
+console.log(sumPrimes(0));
+console.log(sumPrimes(9));
