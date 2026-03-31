7af5108812058bc169c1929b45f13963b53b7362
ayushkothari1 <pahadiayush61@gmail.com>
2026-03-31 19:40:16 +0530

    filter

--- loop/filter.js
+++ loop/filter.js
@@ -7,3 +7,8 @@ console.log(result);
 let arr2 = [1, 2, 3, 4, 5];
 let result2 = arr2.filter((num) => num > 2);
 console.log(result2);
+
+let filterN = [{ a: 1, b: 5 }, { a: 4, b: 0 }, 4, 5];
+let original = filterN.filter((n) => n.a > 1);
+original[0].a = 3;
+console.log(filterN);
